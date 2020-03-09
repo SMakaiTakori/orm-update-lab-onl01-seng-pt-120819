70da4125d0fba9d2a2da0d7db9558c4f4d7b3cdb
S. Makai Takori <makait87@gmail.com>
2020-03-09 15:09:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,13 +4,13 @@ class Student
   
   attr_accessor :name, :grade, :id
   
-    def initialize (name, grade, id= nil)
+  def initialize (name, grade, id= nil)
     @name = name
     @grade = grade
     @id = id
-    end
+  end
     
-    def self.create_table
+def self.create_table
    sql =  <<-SQL
      CREATE TABLE IF NOT EXISTS songs (
        id INTEGER PRIMARY KEY,
