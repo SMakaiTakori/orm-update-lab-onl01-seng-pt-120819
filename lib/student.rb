d4c741a95b39f8322a84a035b5c6b121b82e9f03
S. Makai Takori <makait87@gmail.com>
2020-03-09 15:34:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,12 +49,24 @@ end
     student
   end
 
-def self.new_from_db(row)
+  def self.new_from_db(row)
     # create a new Student object given a row from the database
     student = self.new(row[1], row[2], row[0])
     student
   end  
   
-  
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 end
