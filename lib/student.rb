170250f5b8522b992b624799ee700a8dca7deee8
S. Makai Takori <makait87@gmail.com>
2020-03-09 15:30:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ def save
    self.update
  else
    sql = <<-SQL
-     INSERT INTO songs (name, grade)
+     INSERT INTO students (name, grade)
      VALUES (?, ?)
    SQL
    DB[:conn].execute(sql, self.name, self.grade)
